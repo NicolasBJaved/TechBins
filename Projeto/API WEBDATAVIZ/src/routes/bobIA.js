a6e11be2f6477ab52fcdb4c14ccdab70e0a513cc
Fernanda <fernanda.araujo@sptech.school>
2025-06-05 14:46:39 -0300

    fazendo o BobIA funcionar

--- Projeto/API WEBDATAVIZ/src/routes/bobIA.js
+++ Projeto/API WEBDATAVIZ/src/routes/bobIA.js
@@ -1,9 +1,44 @@
+var express = require("express");
 var router = express.Router();
-var bobIAController = require("../controllers/bobIAController");
 
-router.post("/perguntar", function (req, res) {
+const { GoogleGenAI } = require("@google/genai");
+// configurando o gemini (IA)
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `Em um paragráfo responda: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+router.post("/perguntar", async function (req, res) {
     console.log("Entrou na rota /perguntar");
-    bobIAController.gerarResposta(req, res);
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
 });
 
 
