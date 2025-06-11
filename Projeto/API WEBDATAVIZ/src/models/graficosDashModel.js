6c2d5e72b2d69188f72759a006d177d345506202
guilheNasci <guilherme.mnascimento@sptech.school>
2025-06-11 12:09:56 -0300

    atualizando ambiente de produção e adicionando nova estrutura de CSS para suporte (Refazendo commit antigo)

--- Projeto/API WEBDATAVIZ/src/models/graficosDashModel.js
+++ Projeto/API WEBDATAVIZ/src/models/graficosDashModel.js
@@ -22,9 +22,9 @@ function buscarUltimasMedidas(idGrafico, idEmpresa) {
     if (idGrafico == 2) {
         var instrucaoSql = `SELECT logradouro, round(avg(nivel_lixo),0) as media 
         FROM view_nivellixo
-        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') = 7 AND idEmpresa = ${idEmpresa}
+        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') <= 7 AND idEmpresa = ${idEmpresa}
         GROUP BY logradouro
-        ORDER BY media ASC LIMIT 3;`;
+        ORDER BY media ASC limit 3`;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
     }
@@ -32,7 +32,7 @@ function buscarUltimasMedidas(idGrafico, idEmpresa) {
     if (idGrafico == 3) {
         var instrucaoSql = `SELECT TIME(dataHoraMedicao) as horario, round(avg(nivel_lixo),0) as media 
         FROM view_nivellixo
-        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') = 7 AND idEmpresa = ${idEmpresa}
+        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') = 7 AND idEmpresa = ${idEmpresa} AND TIME(dataHoraMedicao) IN ('00:00:00', '06:00:00', '12:00:00', '18:00:00')
         GROUP BY TIME(dataHoraMedicao)
         ORDER BY horario DESC`;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -124,7 +124,7 @@ function atualizarGrafico(filtroTempo, filtroGrupo, idGrafico, idEmpresa, tempoF
     if (filtroTempo != 'allTime' && idGrafico == 3) {
         var instrucaoSql = `SELECT TIME(dataHoraMedicao) as horario, round(avg(nivel_lixo),0) as media 
         FROM view_nivellixo
-        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') <= ${tempoFiltro} AND idEmpresa = ${idEmpresa}
+        WHERE timestampdiff(day, dataHoraMedicao, '2025-05-30') <= ${tempoFiltro} AND idEmpresa = ${idEmpresa} AND TIME(dataHoraMedicao) IN ('00:00:00', '06:00:00', '12:00:00', '18:00:00')
         GROUP BY TIME(dataHoraMedicao)
         ORDER BY horario DESC`;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -134,7 +134,7 @@ function atualizarGrafico(filtroTempo, filtroGrupo, idGrafico, idEmpresa, tempoF
     if (filtroTempo == 'allTime' && idGrafico == 3) {
         var instrucaoSql = `SELECT TIME(dataHoraMedicao) as horario, round(avg(nivel_lixo),0) as media 
         FROM view_nivellixo
-        WHERE idEmpresa = ${idEmpresa}
+        WHERE idEmpresa = ${idEmpresa} AND TIME(dataHoraMedicao) IN ('00:00:00', '06:00:00', '12:00:00', '18:00:00')
         GROUP BY TIME(dataHoraMedicao)
         ORDER BY horario DESC`;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
